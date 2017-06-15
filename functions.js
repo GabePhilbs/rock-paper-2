33a97649cb54fd11663b8d5a432bc002ffac638f
Gabe <gabriel.philbois@gmail.com>
2017-06-15 18:56:19 -0500

    everything works

--- functions.js
+++ functions.js
@@ -105,26 +105,153 @@
 
 			// when initiating a new game, the array hands must be referenced
 			// also, two places must be chosen from the playerList e.g. (hands,playerList[0],playerList[1])
-			function playGame(hands, player1,player2){
+			function playGame(hands, player1,player2, playUntil){
 
 
 				//the 
 				var players =[player1, player2];
 
 				// the 4th number of this array is the number of round wins required to win the game
-				var gameStatus = [0,0,0,3];
+				var gameStatus = [0,0,0,playUntil];
 
 
+				console.log(" ");
+				console.log(" ");
+				console.log(" ");
+				console.log("		Next match: " + players[0].name + " vs " + players[1].name );
+				console.log(" ");
+				console.log(" ");
 
 
+				//loop while neither player enough victories to win the game
+				while((gameStatus[0] < gameStatus[3] ) && (gameStatus[1] < gameStatus[3])){
 
+					gameStatus = playRound(gameStatus,players,hands);
 
+				}
 
+				if (gameStatus[0] > gameStatus[1]){
+					var gameWinner = players[0];
 
+				} else{
+					var gameWinner = players[1];
 
+				}
+				
 
+				///   * * * I WANTED TO SORT THE SCORE, BUT I'M DOING SOMETHING WRONG * * * 
+				//function sortScore(gameStatus){
+				//	var finalScore = [gameStatus[0],gameStatus[1]];
+				///	 finalScore = finalScore.sort(finalScore);
+				//	 return finalScore;
+
+				//}
+
+				//var finalScore = sortScore(gameStatus);
+
+				//console.log(finalScore);
+				console.log('The winner of this game is ' + gameWinner.name);
+				console.log('The score was ' + gameStatus[0] + " x " + gameStatus[1]);
+				console.log(" ");
+				console.log(" ");
+				console.log(" ");
+
+				return gameWinner;
 
 			}
+
+
+
+
+
+
+
+				// * * MATCHMAKING  * *
+
+
+			// a loop that creates games to acomadate the players
+			function matchmaking (playerList, playUntil){
+
+					//an array of who won the games
+					var winnersOfRound =[];
+
+					//counter for the winners array
+					var i = 0;
+
+					
+
+					//testing if everything is ok so far
+					//console.log(playerList.length)
+					//console.log(playerList.pop());
+					//console.log(playerList);
+
+					//every game takes two players, until there is nobody left
+				while (playerList.length > 0){
+				
+					i++;
+
+					
+
+					//take two players from the list for this match
+					//start a game, and push the winner to the winnersOfRound array
+					winnersOfRound.push( playGame( hands, playerList.pop(), playerList.pop() , playUntil ) );
+
+				}
+
+				return winnersOfRound;
+			}
+
+
+
+
+
+
+
+			// * * THE FINAL TOURNAMENT OF DOOM!!!!!!! * *
+
+
+			//the playTournament function gets the player list and the length chosen for each game
+			function playTournament(playerList,playUntil){
+
+				//next, we need to be sure there is an even number of players
+				if(playerList.length % 2 != 0){
+
+					//if it is odd, tell player
+					console.log("you need an even number of players to have a tournament!");
+					//then stop the function
+					return;
+
+				}
+
+				//if the number is even, the tournament begins.
+				//if we passed the odd/even test, the function continues
+
+				while (playerList.length > 1){
+					playerList = matchmaking(playerList, playUntil);
+				}
+				
+				console.log(playerList[0].name + " is the world champion!");
+				console.log(" ");
+				console.log(" ");
+				
+
+				
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- players.js
+++ players.js
@@ -9,6 +9,15 @@
 
 
 						{
+							name:'Martin',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
 							name:'John',
 							//function to generate hand
 							getHand: function(hands){
@@ -18,7 +27,7 @@
 							}
 						},
 						{
-							name:'Bob',
+							name:'Lee',
 							//function to generate hand
 							getHand: function(hands){
 								hand =genericGetHand(hands);
@@ -26,6 +35,125 @@
 								return hand;
 							}
 						},
+						{
+							name:'Marshall',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
+							name:'Jack',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
+							name:'Edward',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
+							name:'Henry',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
+							name:'Joseph',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
+							name:'Ed',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
+							name:'George',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
+							name:'Robert',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
+							name:'Pierce Brosnan',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
+							name:'Sean Connery',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
+							name:'Roger Moore',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
+							name:'George Lazenby',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+						{
+							name:'Timothy Dalton',
+							//function to generate hand
+							getHand: function(hands){
+								hand =genericGetHand(hands);
+								
+								return hand;
+							}
+						},
+
+
 						]
 
 
